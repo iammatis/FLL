b31c66a50ef38033e9aab9a7de22e748f6fbbed6
iammatis <matej.vilk@gmail.com>
2017-04-23 14:48:38 +0200

    Update to Lunar

---
+++ config/toastr.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'options' => [
+        "debug"=> false,
+        "newestOnTop"=> false,
+        "positionClass"=> "toast-top-center",
+        "closeButton"=> true,
+        "progressBar"=> true
+    ]
+];
