3a7c0d728a358d1c248f9028d7beeb3ca29b094c
iammatis <matej.vilk@gmail.com>
2017-05-13 14:36:53 +0200

    IDs take last number

--- app/Http/Controllers/IDsController.php
+++ app/Http/Controllers/IDsController.php
@@ -17,7 +17,7 @@ class IDsController extends Controller
 
         if ($str = str_contains($ids, '-')) {
         	$splitRange = explode('-', $ids, 2);
-        	for ($i=(int)$splitRange[0]; $i < (int)$splitRange[1]; $i++) {
+        	for ($i=(int)$splitRange[0]; $i <= (int)$splitRange[1]; $i++) {
                 if (IDs::where('fll_id', '=', $i)->exists()) {
                     \Toastr::error('IDs obsahovalo id, ktoré sa v databáze už nachádza!', 'IDs nepridané');
                     return redirect()->route('teams/index');
