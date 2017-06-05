65efb4081a5b5353747fe479f4d87549b379b7ff
root <root@kempelen.ii.fmph.uniba.sk>
2017-06-05 16:55:49 +0200

    adding about page

---
+++ resources/views/web/aboutfll.blade.php
@@ -0,0 +1,35 @@
+@extends('web.layouts.master')
+
+@section('title') Podmienky súťaže @endsection
+
+@section('content')
+
+<section class="content">
+	<div class="container">
+
+		<div class="hr-title hr-long center"><abbr>Podmienky súťaže</abbr> </div>
+
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<h3>Príhlásiť sa môže tím, ktorý spĺňa tieto podmienky:</h3>
+				<ul>
+					<li>má 3 až 10 členov</li>
+					<li>súťažiaci musia byť vo veku 9 až 16 rokov (30. augusta 2016 musí mať každý člen tímu aspoň 9 a menej ako 17 rokov)</li>
+					<li>má 1 dospelého trénera</li>
+					<li>v súťažných robotoch využije iba štandardné komponenty z LEGO stavebníc (a riadiacu jednotku NXT, EV3, alebo RCX) - podrobnosti nájdete v pravidlách</li>
+					<li>programuje v štandardnom softvéri RoboLab, EV3, alebo NXT-G</li>
+				</ul>
+
+				<h3>Každý registrovaný tím musí rátať s finančnými výdavkami, spojenými so súťažou:</h3>
+				<ul>
+					<li>registračný poplatok 135 Eur</li>
+					<li>náklady na jednodňovú cestu na súťaž pre celý tím</li>
+					<li>náklady na stavebnice LEGO Mindstorms, alebo LEGO Mindstorms NXT</li>
+					<li>poplatok 129 Eur za Challenge Set 2016 hradí pre všetky tímy tento rok FLL na Slovensku</li>
+				</ul>
+			</div>
+		</div>
+    </div>
+</section>
+
+@endsection
